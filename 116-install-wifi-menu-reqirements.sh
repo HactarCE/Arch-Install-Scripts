49d1328ffc4763c7e717da8292397c600aba4fbf
HactarCE <6060305+HactarCE@users.noreply.github.com>
2018-08-22 23:43:49 -0400

    Fixed pacman command in 116-install-wifi-menu-reqirements.sh

--- 116-install-wifi-menu-reqirements.sh
+++ 116-install-wifi-menu-reqirements.sh
@@ -6,8 +6,8 @@ echo "${bold}If your computer supports WiFi, you'll probably want to be able to 
 echo "Although ${blue}wifi-menu${white} has already been installed on your system by ${blue}pacstrap${white}, it requires the ${yellow}dialog${white} and ${yellow}wpa_supplicant${white} packages, which probably have not been installed.${nofmt}"
 read -k1 "response?${bold}Install ${yellow}dialog and wpa_supplicant${white}? ${Yn}"
 if [[ ${response:l} != n ]]; then
-  echo "${bold}${blue}pacman -S dialog wpa_supplicant --needed --noconfirm${nofmt}"
-  pacman -S dialog --needed --noconfirm
+  echo "${bold}${blue}arch-chroot /mnt pacman -S dialog wpa_supplicant --needed --noconfirm${nofmt}"
+  arch-chroot /mnt pacman -S dialog wpa_supplicant --needed --noconfirm
   echo
   echo "${bold}${yellow}dialog${greeen} and ${yellow}wpa_supplicant${greeen} package installation complete\!${nofmt}"
 fi
