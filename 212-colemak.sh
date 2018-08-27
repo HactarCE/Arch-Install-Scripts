00946acada218b6222162951157b065eaa1af0e1
HactarCE <6060305+HactarCE@users.noreply.github.com>
2018-08-27 12:19:57 -0400

    Added 212-colemak.sh

---
+++ 212-colemak.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env zsh
+
+source header.sh
+
+read -k1 "response?${bold}This will set the default keyboard layout to Colemak in both the virtual console (TTY) and Xorg. Continue? ${yN}"; echo
+if [[ ${response:l} = y ]]; then
+  layout=`localectl | grep Layout | cut -d: -f2 | cut -b2-`
+  model=`localectl | grep Model | cut -d: -f2 | cut -b2-`
+  echo "${bold}${blue}localectl set-keymap colemak --no-convert${nofmt}"
+  localectl set-keymap colemak --no-convert
+  echo "${bold}${blue}localectl set-x11-keymap $layout $model colemak${nofmt}"
+  localectl set-x11-keymap "$layout" "$model" colemak
+  echo
+  echo "${bold}${green}Colemak keymap configuration complete!${white} You may have to restart before it takes effect.${nofmt}"
+fi
