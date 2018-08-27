0a8385e46eca878ffa76491f756034cdf212f8c3
HactarCE <6060305+HactarCE@users.noreply.github.com>
2018-08-27 11:20:38 -0400

    ttf-ubuntu-font-family is required for lighdm-slick-greeter, not lightdm

--- 214a1-lightdm.sh
+++ 214a1-lightdm.sh
@@ -3,5 +3,5 @@
 source header.sh
 
 echo "${bold}Installing LightDM..."
-pacman_auto_install ttf-ubuntu-font-family lightdm
+pacman_auto_install lightdm
 echo "${bold}${green}LightDM installation mostly complete!${white} You still need a greeter. Run 214a2-lightdm-slick-greeter.sh from a ${underline}non-root${no_underline} account to install ${yellow}lightdm-slick-greeter${white}.${nofmt} (${yellow}lightdm-slick-greeter${white} is on the AUR, and ${blue}makepkg${white} doesn't like running on root accounts.)"
--- 214a2-lightdm-slick-greeter.sh
+++ 214a2-lightdm-slick-greeter.sh
@@ -7,6 +7,11 @@ if [[ $EUID = 0 ]]; then
   return 1
 fi
 
+echo "${bold}Installing ${yellow}ubuntu-font-family${white}..."
+echo
+pacman_auto_install ttf-ubuntu-font-family
+echo
+
 echo "${bold}Installing ${yellow}lightdm-slick-greeter${white} from AUR..."
 echo
 echo "${blue}cd /tmp${nofmt}"
