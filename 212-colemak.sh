9de1c9a80e256766c9e15d6d40be0817ac1c1a74
HactarCE <6060305+HactarCE@users.noreply.github.com>
2019-01-03 17:34:33 -0500

    Enabled shift:both_capslock keyboard option

--- 212-colemak.sh
+++ 212-colemak.sh
@@ -6,10 +6,10 @@ read -k1 "response?${bold}This will set the default keyboard layout to Colemak i
 if [[ ${response:l} = y ]]; then
   layout=`localectl | grep Layout | cut -d: -f2 | cut -b2-`
   model=`localectl | grep Model | cut -d: -f2 | cut -b2-`
-  echo "${bold}${blue}localectl set-keymap colemak --no-convert${nofmt}"
-  localectl set-keymap colemak --no-convert
-  echo "${bold}${blue}localectl set-x11-keymap $layout $model colemak --no-convert${nofmt}"
-  localectl set-x11-keymap "$layout" "$model" colemak --no-convert
+  echo "${bold}${blue}localectl --no-convert set-keymap colemak${nofmt}"
+  localectl --no-convert set-keymap colemak
+  echo "${bold}${blue}localectl --no-convert set-x11-keymap $layout $model colemak shift:both_capslock${nofmt}"
+  localectl --no-convert set-x11-keymap "$layout" "$model" colemak shift:both_capslock
   echo
   echo "${bold}${green}Colemak keymap configuration complete!${white} You may have to restart before it takes effect.${nofmt}"
 fi
