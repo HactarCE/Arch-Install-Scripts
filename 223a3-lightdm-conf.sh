8d79f19f33059265fa2c99007c9fc2050707f961
HactarCE <6060305+HactarCE@users.noreply.github.com>
2019-01-03 19:53:50 -0500

    Fixed 223a3-lightdm-conf.sh to copy files to /etc/lightdm instead of just /etc/lightdm/lightdm.conf.d

--- 223a3-lightdm-conf.sh
+++ 223a3-lightdm-conf.sh
@@ -2,9 +2,9 @@
 
 source header.sh
 
-echo "${bold}This will copy ${yellow}/install-scripts/lightdm-conf.d/*${white} into ${yellow}/etc/lightdm/lightdm.conf.d${white}. If you are not the author of these scripts, ${underline}you should check what those files do before answering \"yes\" to the following prompt.${no_underline}"
+echo "${bold}This will copy ${yellow}/install-scripts/lightdm/*${white} into ${yellow}/etc/lightdm${white}. If you are not the author of these scripts, ${underline}you should check what those files do before answering \"yes\" to the following prompt.${no_underline}"
 echo
-read -k1 "response?${bold}${underline}This will delete all existing files in ${yellow}/etc/lightdm/lightdm.conf.d${white}${no_underline}. Continue? ${Yn}"; echo
+read -k1 "response?${bold}${underline}This will delete all existing files in ${yellow}/etc/lightdm/lightdm.conf.d${white} and may replace some files in ${yellow}/etc/lightdm/${white}${no_underline}. Continue? ${Yn}"; echo
 if [[ ${response:l} != n ]]; then
   if [ -e /etc/lightdm/lightdm.conf.d ]; then
     echo "${bold}${blue}rm -rf /etc/lightdm/lightdm.conf.d${nofmt}"
@@ -12,8 +12,8 @@ if [[ ${response:l} != n ]]; then
   fi
   echo "${bold}${blue}mkdir -p /etc/lightdm/lightdm.conf.d${nofmt}"
   mkdir -p /etc/lightdm/lightdm.conf.d
-  echo "${bold}${blue}cp /install-scripts/lightdm-conf.d/* /etc/lightdm/lightdm.conf.d${nofmt}"
-  cp /install-scripts/lightdm-conf.d/* /etc/lightdm/lightdm.conf.d
+  echo "${bold}${blue}cp -r /install-scripts/lightdm/* /etc/lightdm${nofmt}"
+  cp -r /install-scripts/lightdm/* /etc/lightdm
   echo
   echo "${bold}${green}LightDM configuration complete!${nofmt}"
 fi
