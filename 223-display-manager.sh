4edfcd9eb50b4dd512a4ab2b8bfb5ef57edd4721
HactarCE <6060305+HactarCE@users.noreply.github.com>
2019-01-05 18:52:46 -0500

    Fixed some improper script references

--- 223-display-manager.sh
+++ 223-display-manager.sh
@@ -2,4 +2,4 @@
 
 source header.sh
 
-echo "${bold}If you want to use a display manager other than LightDM, install it yourself and run ${blue}systemctl enable ${yellow}display-manager-service${white}. If you want LightDM, run ${yellow}214a1-lightdm${white}.${nofmt}"
+echo "${bold}If you want to use a display manager other than LightDM, install it yourself and run ${blue}systemctl enable ${yellow}display-manager-service${white}. If you want LightDM, run ${yellow}223a1-lightdm${white}.${nofmt}"
