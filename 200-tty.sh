cdb8aa8663c949fcf0742e65178e1f5669076d86
HactarCE <6060305+HactarCE@users.noreply.github.com>
2019-01-24 22:18:16 -0500

    Fixed a typo

--- 200-tty.sh
+++ 200-tty.sh
@@ -4,4 +4,4 @@ source header.sh
 
 echo "${bold}Congratulations! You're about halfway there."
 echo
-echo "The ${yellow}2xx-${white} scripts should be run from a root account on a live Arch USB.${nofmt}"
+echo "The ${yellow}2xx-${white} scripts should be run from a root account in Arch on the hard disk.${nofmt}"
