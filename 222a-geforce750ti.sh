baf5f272ae1795df3b7730ad46581225355582cd
HactarCE <6060305+HactarCE@users.noreply.github.com>
2019-02-15 20:48:22 -0500

    Added lib32-nvidia-utils for Steam

--- 222a-geforce750ti.sh
+++ 222a-geforce750ti.sh
@@ -5,6 +5,7 @@ source header.sh
 echo "${bold}This script installs the proprietary drivers for Nvidia's GeForce 750 Ti laptop graphics card. If you don't have this card, don't run this script.${nofmt}"
 read -k1 "response?${bold}Install GeForce 750 Ti drivers? ${yN}"; echo
 if [[ ${response:l} = y ]]; then
-  pacman_auto_install mesa nvidia nvidia-utils nvidia-settings
+  pacman_auto_install mesa mesa-demos nvidia nvidia-utils nvidia-settings lib32-nvidia-utils
+  # lib32-nvidia-utils is required for Steam to work (https://wiki.archlinux.org/index.php/Steam#Installation)
   echo "${bold}${green}Nvidia GeForce 750 Ti proprietary drivers installed!${white}"
 fi
