862a9d5b6a0e55efa247c33268f3d9c550c0a307
HactarCE <6060305+HactarCE@users.noreply.github.com>
2019-06-16 23:26:51 -0400

    Added a TODO and a Pacman config suggestion

--- 213-pacman-conf.sh
+++ 213-pacman-conf.sh
@@ -6,5 +6,6 @@ echo "${bold}Pacman settings are stored in ${yellow}/etc/pacman.conf${white}."
 echo
 echo "To enable ${yellow}multilib${white} repositories, uncomment the whole ${yellow}[multilib]${white} section and run ${blue}pacman -Syy${white}."
 echo "To enable color by default, uncomment the ${yellow}Color${white} line in the ${yellow}[options]${white} section."
+echo "I also recommend the ${yellow}TotalDownload${white} option."
 echo
 echo "Good luck!${nofmt}"
