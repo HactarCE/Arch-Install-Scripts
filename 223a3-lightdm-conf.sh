31e74f45d352d290106102b7454bf8dc2fccdb3f
HactarCE <6060305+HactarCE@users.noreply.github.com>
2019-08-08 20:12:59 -0400

    Fixed minor inconsistency

--- 223a3-lightdm-conf.sh
+++ 223a3-lightdm-conf.sh
@@ -4,7 +4,7 @@ source header.sh
 
 echo "${bold}This will copy ${yellow}/install-scripts/etc/lightdm/*${white} into ${yellow}/etc/lightdm${white}. If you are not the author of these scripts, ${underline}you should check what those files do before answering \"yes\" to the following prompt.${no_underline}"
 echo
-read -k1 "response?${bold}${underline}This will delete all existing files in ${yellow}/etc/lightdm/lightdm.conf.d${white} and may replace some files in ${yellow}/etc/lightdm/${white}${no_underline}. Continue? ${Yn}"; echo
+read -k1 "response?${bold}${underline}This will delete all existing files in ${yellow}/etc/lightdm/lightdm.conf.d${white} and may replace some files in ${yellow}/etc/lightdm${white}${no_underline}. Continue? ${Yn}"; echo
 if [[ ${response:l} != n ]]; then
   if [ -e /etc/lightdm/lightdm.conf.d ]; then
     echo "${bold}${blue}rm -rf /etc/lightdm/lightdm.conf.d${nofmt}"
