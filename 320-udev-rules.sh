25d1420d472ef1d6785ddc25dabcd3e37fe77594
HactarCE <6060305+HactarCE@users.noreply.github.com>
2019-08-20 18:01:54 -0400

    Fixed 320-udev-rules.sh

--- 320-udev-rules.sh
+++ 320-udev-rules.sh
@@ -7,13 +7,13 @@ echo
 read -k1 "response?${bold}${underline}This will delete all existing files in ${yellow}/etc/udev/rules.d${white}. Continue? ${Yn}"; echo
 if [[ ${response:l} != n ]]; then
   if [ -e /etc/udev/rules.d ]; then
-    echo "${bold}${blue}rm -rf /etc/udev/rules.d${nofmt}"
+    echo "${bold}${blue}sudo rm -rf /etc/udev/rules.d${nofmt}"
     sudo rm -rf /etc/udev/rules.d
   fi
-  echo "${bold}${blue}mkdir -p /etc/udev/rules.d${nofmt}"
+  echo "${bold}${blue}sudo mkdir -p /etc/udev/rules.d${nofmt}"
   sudo mkdir -p /etc/udev/rules.d
-  echo "${bold}${blue}cp -r /install-scripts/etc/udev/rules.d/* /etc/udev/rules.d${nofmt}"
-  sudo cp -r /install-scripts/etc/udev/rules.d/* /etc/udev/rules.d
+  echo "${bold}${blue}sudo cp /install-scripts/etc/udev/rules.d/* /etc/udev/rules.d${nofmt}"
+  sudo cp /install-scripts/etc/udev/rules.d/* /etc/udev/rules.d
   echo
   echo "${bold}${green}Udev configuration complete!${nofmt}"
 fi
