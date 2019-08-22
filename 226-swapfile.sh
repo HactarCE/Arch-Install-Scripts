4c9836e1d4918439c8843750f4c71b275b7e66fa
HactarCE <6060305+HactarCE@users.noreply.github.com>
2019-08-22 17:11:39 -0400

    Fixed swapfile

--- 226-swapfile.sh
+++ 226-swapfile.sh
@@ -5,5 +5,7 @@ source header.sh
 pacman_auto_install systemd-swap
 echo "${bold}${blue}sudo sed -i 's/swapfc_enabled=0/swapfc_enabled=1/' /etc/systemd/swap.conf${nofmt}"
 sed -i 's/swapfc_enabled=0/swapfc_enabled=1/' /etc/systemd/swap.conf
+echo "${bold}${blue}systemctl enable --now systemd-swap${nofmt}"
+systemctl enable --now systemd-swap
 
 echo "${bold}${green}Swapfile enabled!${nofmt}"
